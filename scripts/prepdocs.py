b2dac18c9eace4f5c28b9ec8c7ca20afbde5d3af
Victor Vazquez <vhvb1989@gmail.com>
2023-03-13 19:51:31 -0700

    Using tenant id as argument for prepdocs

--- scripts/prepdocs.py
+++ scripts/prepdocs.py
@@ -26,6 +26,7 @@ parser.add_argument("--skipblobs", action="store_true", help="Skip uploading ind
 parser.add_argument("--storageaccount", help="Azure Blob Storage account name")
 parser.add_argument("--container", help="Azure Blob Storage container name")
 parser.add_argument("--storagekey", required=False, help="Optional. Use this Azure Blob Storage account key instead of the current user identity to login (use az login to set current user for Azure)")
+parser.add_argument("--tenantid", required=False, help="Optional. Use this to define the Azure directory where to authenticate)")
 parser.add_argument("--searchservice", help="Name of the Azure Cognitive Search service where content should be indexed (must exist already)")
 parser.add_argument("--index", help="Name of the Azure Cognitive Search index where content should be indexed (will be created if it doesn't exist)")
 parser.add_argument("--searchkey", required=False, help="Optional. Use this Azure Cognitive Search account key instead of the current user identity to login (use az login to set current user for Azure)")
@@ -35,8 +36,8 @@ parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output
 args = parser.parse_args()
 
 # Use the current user identity to connect to Azure services unless a key is explicitly set for any of them
-tenant_id = os.environ['AZURE_TENANT_ID']
-default_creds = AzureDeveloperCliCredential(tenant_id=tenant_id) if args.searchkey == None or args.storagekey == None else None
+azd_credential = AzureDeveloperCliCredential() if args.tenantid == None else AzureDeveloperCliCredential(tenant_id=args.tenantid)
+default_creds = azd_credential if args.searchkey == None or args.storagekey == None else None
 search_creds = default_creds if args.searchkey == None else AzureKeyCredential(args.searchkey)
 if not args.skipblobs:
     storage_creds = default_creds if args.storagekey == None else args.storagekey
