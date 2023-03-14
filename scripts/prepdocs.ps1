19ba3a32a15ae69fd8b7300dc4b4764c99604297
Johan Stenberg <johan.stenberg@microsoft.com>
2023-03-14 14:54:06 -0700

    Install scripts into scripts/.venv virtual environment. Add sh shell support for azd hooks when running posix/non-windows OS

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -8,5 +8,6 @@ foreach ($line in $output) {
 
 Write-Host "Environment variables set."
 
-pip install -r ./scripts/requirements.txt
-python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
+python -m venv scripts/.venv
+scripts/scripts/.venv/scripts/python pip install -r ./scripts/requirements.txt
+scripts/scripts/.venv/scripts/python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
