ab5d3e8074b668f259012b9c00563069aea2d4a6
Johan Stenberg (MSFT) <johan.stenberg@microsoft.com>
2023-03-14 15:48:07 -0700

    Update prepdocs.ps1

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -9,5 +9,5 @@ foreach ($line in $output) {
 Write-Host "Environment variables set."
 
 python -m venv scripts/.venv
-scripts/scripts/.venv/scripts/python pip install -r ./scripts/requirements.txt
-scripts/scripts/.venv/scripts/python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
+scripts/.venv/scripts/python pip install -r ./scripts/requirements.txt
+scripts/.venv/scripts/python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
