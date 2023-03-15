0eed6114d021618f05e562cd89d5c12746cb3e71
Alex Zeltov <azeltov@gmail.com>
2023-03-15 16:56:52 -0400

    Update prepdocs.ps1
    need to switch for pwsh windows forward slash './data/*'  otherwise no data is processed
    
    PS /Users/alexzeltov/git/azure-search-openai-demo> python ./scripts/prepdocs.py '.\data\*' --searchkey "XZnvXI20iBoBfucFfsObjMTvVYXny0txh6n81NvwnGAzSeBUiHAL" --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
    Ensuring search index gptkbindex exists
    Search index gptkbindex already exists
    Processing files...
    PS /Users/alexzeltov/git/azure-search-openai-demo> python ./scripts/prepdocs.py './data/*' --searchkey "XZnvXI20iBoBfucFfsObjMTvVYXny0txh6n81NvwnGAzSeBUiHAL" --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v 
    Ensuring search index gptkbindex exists
    Search index gptkbindex already exists
    Processing files...
    Processing './data/Northwind_Standard_Benefits_Details.pdf'
            Uploading blob for page 0 -> Northwind_Standard_Benefits_Details-0.pdf
            Uploading blob for page 1 -> Northwind_Standard_Benefits_Details-1.pdf

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -9,4 +9,4 @@ foreach ($line in $output) {
 Write-Host "Environment variables set."
 
 pip install -r ./scripts/requirements.txt
-python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
+python ./scripts/prepdocs.py './data/*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
