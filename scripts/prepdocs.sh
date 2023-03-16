588583442a722fb197076f0988be714ec29b9edf
Jon Gallant <2163001+jongio@users.noreply.github.com>
2023-03-16 15:02:51 -0700

    Fix up prepdocs.sh

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
 
-if ! command -v python -v &> /dev/null
+if ! command -v python &> /dev/null
 then
     echo 'Python not found on $PATH'
     exit -1
@@ -22,4 +22,4 @@ echo 'Installing dependencies from "requirements.txt" into virtual environment'
 ./scripts/.venv/bin/python -m pip install -r scripts/requirements.txt
 
 echo 'Running "prepdocs.py"'
-./scripts/.venv/bin/python scripts/prepdocs.py './data/*' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" -v
+./scripts/.venv/bin/python ./scripts/prepdocs.py './data/*' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" -v
