2cde6c94c48a0f4e05f2cb9a80a96b6d44caf753
Victor Vazquez <vhvb1989@gmail.com>
2023-03-17 13:58:42 -0700

    support lin/win from prepdocs hook

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -12,11 +12,25 @@ foreach ($line in $output) {
 
 Write-Host "Environment variables set."
 
+$pythonCmd = Get-Command python -ErrorAction SilentlyContinue
+if (-not $pythonCmd) {
+  # fallback to python3 if python not found
+  $pythonCmd = Get-Command python3 -ErrorAction SilentlyContinue
+}
+
 Write-Host 'Creating python virtual environment "scripts/.venv"'
-python -m venv ./scripts/.venv
+Start-Process -FilePath ($pythonCmd).Source -ArgumentList "-m venv ./scripts/.venv" -Wait
+
+$venvPath = "scripts"
+if (-not (Test-Path -Path "./scripts/.venv/$venvPath/python")) {
+  # fallback to Linux venv path
+  $venvPath = "bin"
+} 
 
 Write-Host 'Installing dependencies from "requirements.txt" into virtual environment'
-./scripts/.venv/scripts/python -m pip install -r ./scripts/requirements.txt
+$venvPythonPath = "./scripts/.venv/$venvPath/python"
+Start-Process -FilePath $venvPythonPath -ArgumentList "-m pip install -r ./scripts/requirements.txt" -Wait
 
 Write-Host 'Running "prepdocs.py"'
-./scripts/.venv/scripts/python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX --tenantid $env:AZURE_TENANT_ID -v
+$cdw = (Get-Location)
+Start-Process -FilePath $venvPythonPath -ArgumentList "./scripts/prepdocs.py $cdw/data/* --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX --tenantid $env:AZURE_TENANT_ID -v" -Wait
