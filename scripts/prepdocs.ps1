854d9a28f1fe2120aeeb0834afc53fa14dea83e8
lordaouy <110788250+lordaouy@users.noreply.github.com>
2023-03-20 19:19:51 +0700

    Update prepdocs.ps1
    Update a script to activate my venv

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -7,6 +7,6 @@ foreach ($line in $output) {
 }
 
 Write-Host "Environment variables set."
-
+& ./.venv/bin/Activate.ps1
 pip install -r ./scripts/requirements.txt
 python ./scripts/prepdocs.py '.\data\*' --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --index $env:AZURE_SEARCH_INDEX -v
