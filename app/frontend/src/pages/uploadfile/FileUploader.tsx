7692fb94c49beb5e57c469e74a1bfd6b83752266
Sunil Sattiraju <sunilsattiraju@gmail.com>
2023-03-24 12:34:08 +0800

    add status mesg and align imports

--- app/frontend/src/pages/uploadfile/FileUploader.tsx
+++ app/frontend/src/pages/uploadfile/FileUploader.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import { uploadApi, UploadFileRequest, UploadFileResponse } from "../../api";
-import styles from "./FileUploader.module.css";
+import { useRef, useState, useEffect } from "react";
 import { PrimaryButton } from "@fluentui/react";
+
+import styles from "./FileUploader.module.css";
+
+import { uploadApi, UploadFileRequest, UploadFileResponse } from "../../api";
 
 const FileUploader = () => {
     const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
     const [uploadStatus, setUploadStatus] = useState<UploadFileResponse[]>([]);
+    const lastUploadRef = useRef<string>("");
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<unknown>();
+
+    const handleStartOver = () => {
+        setSelectedFiles(null);
+        setUploadStatus([]);
+    };
+
     const handleFileSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
         const formData = new FormData();
         try {
@@ -18,13 +30,16 @@ const FileUploader = () => {
             }
         } catch (error) {
             console.log(error);
+            handleStartOver();
         }
     };
+
     const handleUpload = async () => {
         if (!selectedFiles) {
             console.log("No files selected");
             return;
         }
+        setIsLoading(true);
         const formData = new FormData();
         for (let i = 0; i < selectedFiles.length; i++) {
             formData.append("file", selectedFiles[i]);
@@ -35,26 +50,33 @@ const FileUploader = () => {
             const data: UploadFileResponse = await response;
             setUploadStatus(prevStatus => [...prevStatus, data]);
             setSelectedFiles(null); // reset the stat to previous after upload complete
+            setIsLoading(false);
         } catch (error: unknown) {
             if (error instanceof Error) {
                 const e: Error = error;
                 setUploadStatus(prevStatus => [...prevStatus, { success: false, message: e.message }]);
             }
-            setSelectedFiles(null);
+            handleStartOver();
+            setIsLoading(false);
         }
     };
+
     let statusMessage: string | null = null;
-    if (uploadStatus.length > 0) {
+
+    if (isLoading) {
+        statusMessage = "Uploading...";
+    } else if (uploadStatus.length > 0) {
         const successCount = uploadStatus.filter(status => status.success).length;
         if (successCount === uploadStatus.length) {
             statusMessage = "Upload successful!";
         } else {
-            statusMessage = `${successCount} of ${uploadStatus.length} files uploaded successfully\n ${uploadStatus
+            statusMessage = `${uploadStatus
                 .filter(status => !status.success)
                 .map(status => status.message)
                 .join(", ")}`;
         }
     }
+
     return (
         <div className={styles.uploadContainer}>
             <div className={styles.uploadTopSection}>
@@ -71,4 +93,5 @@ const FileUploader = () => {
         </div>
     );
 };
+
 export default FileUploader;
