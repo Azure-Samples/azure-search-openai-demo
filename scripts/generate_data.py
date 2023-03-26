66e8f0fe34bafca2451568f7236f3d1fbcdcf657
sindre-bouvet <sindre.lange@bouvet.no>
2023-03-26 12:08:27 +0200

    Comment out mp4 -> mp3, transcribe the videos directly

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,13 +1,16 @@
 from utils.whisper import transcribe
 from utils.download_yt import download_video
+from utils.convert_yt import mp4_to_mp3
 
 if __name__ == '__main__':
     with open('data/links/huberman.txt', 'r') as f:
         for link in f.readlines():
-            file_processed_path = download_video(link)
-            transcribed = transcribe(file_processed_path)
+            video_processed_path = download_video(link)
+            # audio_processed_path = video_processed_path.replace('video', 'audio').replace('.mp4', '.mp3')
+            # mp4_to_mp3(video_processed_path, audio_processed_path)
+            transcribed = transcribe(video_processed_path)
             if transcribed:
-                print(f"Successfully transcribed {file_processed_path}")
+                print(f"Successfully transcribed {video_processed_path}")
             else:
-                print(f"Unable to transcribe {file_processed_path}")
+                print(f"Unable to transcribe {video_processed_path}")
         f.close()
