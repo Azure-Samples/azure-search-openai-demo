19e872fa0713e7c2449fcf8eaa7be76c0b616439
sindre-bouvet <sindre.lange@bouvet.no>
2023-03-26 12:06:28 +0200

    Change only name of file, not the entire path

--- scripts/utils/download_yt.py
+++ scripts/utils/download_yt.py
@@ -4,9 +4,16 @@ import os
 def download_video(link):
     yt = YouTube(link)
     print(f"Downloading: {link}")
-    filename_initial = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download('data/video/')
-    filename_processed = filename_initial.replace(' ', '_')
-    filename_processed = filename_processed.lower()
-    print(f"Renaming, from {filename_initial} to {filename_processed}")
-    os.rename(filename_initial, filename_processed)
-    return filename_processed
+    filepath_initial = (yt
+                        .streams
+                        .filter(progressive=True, file_extension='mp4')
+                        .order_by('resolution')
+                        .asc()
+                        .first()
+                        .download('data/video/'))
+    filename_initial = filepath_initial.split('/')[-1]
+    filename_processed = (filename_initial.replace(' ', '_')).lower()
+    filepath_processed = '/'.join(filepath_initial.split('/')[:-1]) + f"/{filename_processed}"
+    print(f"Renaming, from {filepath_initial} to {filepath_processed}")
+    os.rename(filepath_initial, filepath_processed)
+    return filepath_processed
