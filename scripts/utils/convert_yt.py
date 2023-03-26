5b183d2bcad5c2ba660abea2884a0c8f951fa421
sindre-bouvet <sindre.lange@bouvet.no>
2023-03-26 12:09:39 +0200

    Make convert from mp4 to mp3 a callable function

--- scripts/utils/convert_yt.py
+++ scripts/utils/convert_yt.py
@@ -1,12 +1,12 @@
 from moviepy.editor import AudioFileClip
 
 def mp4_to_mp3(mp4, mp3):
-    FILETOCONVERT = AudioFileClip(mp4)
-    FILETOCONVERT.write_audiofile(mp3)
-    FILETOCONVERT.close()
-
-if __name__ == '__main__':
-    VIDEO_FILE_PATH = "<>"
-    AUDIO_FILE_PATH = "<>"
-
-    mp4_to_mp3(VIDEO_FILE_PATH, AUDIO_FILE_PATH)
+    try:
+        file_to_convert = AudioFileClip(mp4)
+        file_to_convert.write_audiofile(mp3)
+        file_to_convert.close()
+    except Exception as exception:
+        print(f"Error when converting {mp4} to {mp3}: \n{exception}")
+        return False
+    
+    return True
