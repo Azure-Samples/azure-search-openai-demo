70f4ad1bc320f6ace237c5d75eb6b228763e2f4f
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2023-04-06 10:49:06 -0700

    Update app/backend-dotnet/Services/RetrieveThenReadApproachService.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- app/backend-dotnet/Services/RetrieveThenReadApproachService.cs
+++ app/backend-dotnet/Services/RetrieveThenReadApproachService.cs
@@ -10,33 +10,34 @@ namespace IMJohn.Services
     public class RetrieveThenReadApproachService : IApproachService
     {
         private readonly SearchClient searchClient;
-        private readonly string TEMPLATE = @"
-You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions.
-Use 'you' to refer to the individual asking the questions even if they ask with 'I'.
-Answer the following question using only the data provided in the sources below.
-For tabular information return it as an html table. Do not return markdown format.
-Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response.
-If you cannot answer using the sources below, say you don't know.
-
-###
-Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
-
-Sources:
-info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
-info2.pdf: Overlake is in-network for the employee plan.
-info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
-info4.pdf: In-network institutions include Overlake, Swedish and others in the region
-
-Answer:
-In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].
-
-###
-Question: {{$question}}?
-
-Sources:
-{{$retreive}}
-
-Answer:";
+        private const string TEMPLATE = """
+          You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions.
+          Use 'you' to refer to the individual asking the questions even if they ask with 'I'.
+          Answer the following question using only the data provided in the sources below.
+          For tabular information return it as an HTML table. Do not return markdown format.
+          Each source has a name followed by a colon and the actual information, always include the source name for each fact you use in the response.
+          If you cannot answer using the sources below, say you don't know.
+          
+          ###
+          Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
+          
+          Sources:
+          info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employees and $1000 for families. Out-of-network deductibles are $1000 for employees and $2000 for families.
+          info2.pdf: Overlake is in-network for the employee plan.
+          info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
+          info4.pdf: In-network institutions include Overlake, Swedish, and others in the region
+          
+          Answer:
+          In-network deductibles are $500 for employees and $1000 for families [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].
+          
+          ###
+          Question: {{$question}}?
+          
+          Sources:
+          {{$retreive}}
+          
+          Answer:
+          """;
         private readonly IKernel kernel;
         private readonly ISKFunction function;
 
