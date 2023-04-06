f06152058daf4af4f5ada284a329e9573ca0c09c
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-04-06 11:53:23 -0700

    revert change in frontend

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-var useDotnet = process.env.AZURE_SEARCH_OPENAI_USE_DOTNET == "true" ? true : false;
-var deployPath = useDotnet ? "../backend-dotnet/wwwroot" : "../backend/static";
-console.log(`deploy build to ${deployPath}`);
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: deployPath,
+        outDir: "../backend/wwwroot",
         emptyOutDir: true,
         sourcemap: true
     },
