10becb4909a9948949bb185aec1020a3b9a4708a
Brady Leavitt <brady.d.leavitt@gmail.com>
2023-05-19 00:15:33 -0400

    fixed header for to check for auth user

--- app/backend/auth/auth_utils.py
+++ app/backend/auth/auth_utils.py
@@ -1,14 +1,14 @@
-def get_authenticated_user_details(request):
+def get_authenticated_user_details(request_headers):
     user_object = {}
 
     ## check the headers for the Principal-Id (the guid of the signed in user)
-    if "X-Ms-Client-Principal-Id" not in request.headers:
+    if "X-Ms-Client-Principal-Id" not in request_headers.keys():
         ## if it's not, assume we're in development mode and return a default user
         from auth.sample_user import sample_user
         raw_user_object = sample_user
     else:
         ## if it is, get the user details from the EasyAuth headers
-        raw_user_object = {k:v for k,v in request.headers.items()}
+        raw_user_object = {k:v for k,v in request_headers.items()}
 
     user_object['user_principal_id'] = raw_user_object['X-Ms-Client-Principal-Id']
     user_object['user_name'] = raw_user_object['X-Ms-Client-Principal-Name']
