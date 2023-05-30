dfedc3976d553d86dd80842937eaf30c39a509a3
lummy2020 <135025088+lummy2020@users.noreply.github.com>
2023-05-30 09:19:06 -0400

    Update Layout.tsx

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>Brookfield Asset Management | Technology Service</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -38,7 +38,7 @@ const Layout = () => {
                             </li>
                         </ul>
                     </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + Cognitive Search</h4>
+                    <h4 className={styles.headerRightText}>Brookfield Data + Cognitive Search</h4>
                 </div>
             </header>
 
