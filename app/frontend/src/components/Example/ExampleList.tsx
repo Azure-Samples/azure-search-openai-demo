b42ff9ce03c461308fab0ad0a7b506067d22a669
lummy2020 <135025088+lummy2020@users.noreply.github.com>
2023-05-30 09:27:59 -0400

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What was Brookfield revenue in 2022?",
+        value: "What was Brookfield revenue in 2022?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What was Brookfield revenue in 2023?", value: "What was Brookfield revenue in 2023?" },
+    { text: "what is brookfield investment apporach?", value: "what is brookfield investment apporach?" }
 ];
 
 interface Props {
