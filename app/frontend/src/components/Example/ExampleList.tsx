5488f84a586c69d7f2814e0761989379a5f5d6f3
Cesar Quebral <109699792+bpccquebral@users.noreply.github.com>
2023-06-04 23:35:27 +0000

    Updated prompts from Jeremy.

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "Give me a detailed executive summary of APEX. Give it to me in a way that a credit analyst at hedge fund would want to know. Provide sources.",
-        value: "Give me a detailed executive summary of APEX. Give it to me in a way that a credit analyst at hedge fund would want to know. Provide sources."
+        text: "Create a one page summary of APEX tools.",
+        value: "Create a detailed one pager summary for credit hedge fund analyst for apex tool using 2023 Q1 and other latest data and listing sources. Looks like: Company overview: Overview here Capital Structure: Capital Structure here and list the amount of each item. For Term Loan and Revolving Credit Facility look at Apex Tool Group Q1 2023 Financial Capitalization & Liquidity: Metrics here and show trends Financial Results and Estimates: results here and include last 4 quarters Positive Trends: Trends here Negative Trends and Risks: Trends and risk here and elaborate more on the risk factors and list numbers Loan Covenants: Covenants here and list important covenants to credit analyst. List numeric amount of threshold and current number."
     },
-    { text: "What are the positive and negative trends about Apex Tool?", value: "What are the positive and negative trends about Apex Tool?" },
-    { text: "What's the latest data available for Apex Tool?", value: "What's the latest data available for Apex Tool?" }
+    { text: "What drives Apex Tool's EBITDA change in the last 4 quarters?", value: "What drives Apex Tool's EBITDA change in the last 4 quarters? Give a detailed response that a credit analyst at a hedge fund would want." },
+    { text: "Show calculation of Apex Tool's adjusted EBITDA for Q1 2023.", value: "Show calculation of Apex Tool's adjusted EBITDA for Q1 2023, Give a detailed response that a credit analyst at a hedge fund would want." }
 ];
 
 interface Props {
