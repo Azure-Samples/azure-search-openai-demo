b436c54b72f5bab86b753037328db1a44fd633d1
piassa157 <lindaum98@gmail.com>
2023-06-20 22:59:17 -0300

    fix: validate python version.

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -14,10 +14,16 @@ $(azd env get-values)
 EOF
 
 echo 'Creating python virtual environment "scripts/.venv"'
-python -m venv $current_dir/scripts/.venv
+
+if command -v python3 &>/dev/null; then
+    python3 -m venv $current_dir/scripts/.venv
+else
+    python -m venv $current_dir/scripts/.venv
+fi
+
 
 echo 'Installing dependencies from "requirements.txt" into virtual environment'
-./scripts/.venv/bin/python -m pip install -r $current_dir/scripts/requirements.txt
+./scripts/.venv/bin/python -m pip install -r $current_dir/requirements.txt
 
 echo 'Running "prepdocs.py"'
-./scripts/.venv/bin/python $current_dir/scripts/prepdocs.py './data/*' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" --formrecognizerservice "$AZURE_FORMRECOGNIZER_SERVICE" --tenantid "$AZURE_TENANT_ID" -v
+./scripts/.venv/bin/python $current_dir/predocs.py './data/*' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" --formrecognizerservice "$AZURE_FORMRECOGNIZER_SERVICE" --tenantid "$AZURE_TENANT_ID" -v
