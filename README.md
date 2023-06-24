b59f630b4d95a64c6351a12918fbcb3c27a8135b
juju4 <juju4@users.noreply.github.com>
2023-06-24 18:32:28 +0000

    Revert "chore: fix pre-commit eof, trailing whitespaces, pretty json, codespell"
    This reverts commit d970618874291998f9a4802d4d93cabdac5cbc39.

--- README.md
+++ README.md
@@ -30,13 +30,13 @@ The repo includes sample data so it's ready to try end to end. In this sample ap
 - [Azure Developer CLI](https://aka.ms/azure-dev/install)
 - [Python 3+](https://www.python.org/downloads/)
     - **Important**: Python and the pip package manager must be in the path in Windows for the setup scripts to work.
-    - **Important**: Ensure you can run `python --version` from console. On Ubuntu, you might need to run `sudo apt install python-is-python3` to link `python` to `python3`.
+    - **Important**: Ensure you can run `python --version` from console. On Ubuntu, you might need to run `sudo apt install python-is-python3` to link `python` to `python3`.    
 - [Node.js](https://nodejs.org/en/download/)
 - [Git](https://git-scm.com/downloads)
 - [Powershell 7+ (pwsh)](https://github.com/powershell/powershell) - For Windows users only.
    - **Important**: Ensure you can run `pwsh.exe` from a PowerShell command. If this fails, you likely need to upgrade PowerShell.
 
->NOTE: Your Azure Account must have `Microsoft.Authorization/roleAssignments/write` permissions, such as [User Access Administrator](https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#user-access-administrator) or [Owner](https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#owner).
+>NOTE: Your Azure Account must have `Microsoft.Authorization/roleAssignments/write` permissions, such as [User Access Administrator](https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#user-access-administrator) or [Owner](https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#owner).  
 
 #### To Run in GitHub Codespaces or VS Code Remote Containers
 
@@ -60,12 +60,12 @@ You can run this repo virtually by using GitHub Codespaces or VS Code Remote Con
 Execute the following command, if you don't have any pre-existing Azure services and want to start from a fresh deployment.
 
 1. Run `azd up` - This will provision Azure resources and deploy this sample to those resources, including building the search index based on the files found in the `./data` folder.
-1. After the application has been successfully deployed you will see a URL printed to the console.  Click that URL to interact with the application in your browser.
+1. After the application has been successfully deployed you will see a URL printed to the console.  Click that URL to interact with the application in your browser.  
 
 It will look like the following:
 
 !['Output from running azd up'](assets/endpoint.png)
-
+    
 > NOTE: It may take a minute for the application to be fully deployed. If you see a "Python Developer" welcome screen, then wait a minute and refresh the page.
 
 #### Use existing resources:
@@ -124,4 +124,4 @@ Once in the web app:
 
 If you see this error while running `azd deploy`: `read /tmp/azd1992237260/backend_env/lib64: is a directory`, then delete the `./app/backend/backend_env folder` and re-run the `azd deploy` command.  This issue is being tracked here: https://github.com/Azure/azure-dev/issues/1237
 
-If the web app fails to deploy and you receive a '404 Not Found' message in your browser, run 'azd deploy'.
+If the web app fails to deploy and you receive a '404 Not Found' message in your browser, run 'azd deploy'. 
--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -12,7 +12,7 @@ from text import nonewlines
 from lookuptool import CsvLookupTool
 
 # Attempt to answer questions by iteratively evaluating the question to see what information is missing, and once all information
-# is present then formulate an answer. Each iteration consists of two parts: first use GPT to see if we need more information,
+# is present then formulate an answer. Each iteration consists of two parts: first use GPT to see if we need more information, 
 # second if more data is needed use the requested "tool" to retrieve it. The last call to GPT answers the actual question.
 # This is inspired by the MKRL paper[1] and applied here using the implementation in Langchain.
 # [1] E. Karpas, et al. arXiv:2205.00445
@@ -29,13 +29,13 @@ class ReadRetrieveReadApproach(Approach):
 "Never quote tool names as sources." \
 "If you cannot answer using the sources below, say that you don't know. " \
 "\n\nYou can access to the following tools:"
-
+    
     template_suffix = """
 Begin!
 
 Question: {input}
 
-Thought: {agent_scratchpad}"""
+Thought: {agent_scratchpad}"""    
 
     CognitiveSearchToolDescription = "useful for searching the Microsoft employee benefits information such as healthcare plans, retirement plans, etc."
 
@@ -53,11 +53,11 @@ Thought: {agent_scratchpad}"""
 
         if overrides.get("semantic_ranker"):
             r = self.search_client.search(q,
-                                          filter=filter,
-                                          query_type=QueryType.SEMANTIC,
-                                          query_language="en-us",
-                                          query_speller="lexicon",
-                                          semantic_configuration_name="default",
+                                          filter=filter, 
+                                          query_type=QueryType.SEMANTIC, 
+                                          query_language="en-us", 
+                                          query_speller="lexicon", 
+                                          semantic_configuration_name="default", 
                                           top = top,
                                           query_caption="extractive|highlight-false" if use_semantic_captions else None)
         else:
@@ -68,7 +68,7 @@ Thought: {agent_scratchpad}"""
             self.results = [doc[self.sourcepage_field] + ":" + nonewlines(doc[self.content_field][:250]) for doc in r]
         content = "\n".join(self.results)
         return content
-
+        
     def run(self, q: str, overrides: dict) -> any:
         # Not great to keep this as instance state, won't work with interleaving (e.g. if using async), but keeps the example simple
         self.results = None
@@ -76,9 +76,9 @@ Thought: {agent_scratchpad}"""
         # Use to capture thought process during iterations
         cb_handler = HtmlCallbackHandler()
         cb_manager = CallbackManager(handlers=[cb_handler])
-
-        acs_tool = Tool(name="CognitiveSearch",
-                        func=lambda q: self.retrieve(q, overrides),
+        
+        acs_tool = Tool(name="CognitiveSearch", 
+                        func=lambda q: self.retrieve(q, overrides), 
                         description=self.CognitiveSearchToolDescription,
                         callbacks=cb_manager)
         employee_tool = EmployeeInfoTool("Employee1", callbacks=cb_manager)
@@ -93,11 +93,11 @@ Thought: {agent_scratchpad}"""
         chain = LLMChain(llm = llm, prompt = prompt)
         agent_exec = AgentExecutor.from_agent_and_tools(
             agent = ZeroShotAgent(llm_chain = chain, tools = tools),
-            tools = tools,
-            verbose = True,
+            tools = tools, 
+            verbose = True, 
             callback_manager = cb_manager)
         result = agent_exec.run(q)
-
+                
         # Remove references to tool names that might be confused with a citation
         result = result.replace("[CognitiveSearch]", "").replace("[Employee]", "")
 
@@ -107,9 +107,9 @@ class EmployeeInfoTool(CsvLookupTool):
     employee_name: str = ""
 
     def __init__(self, employee_name: str, callbacks: Callbacks = None):
-        super().__init__(filename="data/employeeinfo.csv",
-                         key_field="name",
-                         name="Employee",
+        super().__init__(filename="data/employeeinfo.csv", 
+                         key_field="name", 
+                         name="Employee", 
                          description="useful for answering questions about the employee, their benefits and other personal information",
                          callbacks=callbacks)
         self.func = self.employee_info
