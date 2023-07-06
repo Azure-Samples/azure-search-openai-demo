c2e9a9b5033a4dfb1db6d8aaacc4ec0e732ea047
Alok0406 <57394369+Alok0406@users.noreply.github.com>
2023-07-06 15:44:20 +0000

    header update

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>OneCloud AutomationHub</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
