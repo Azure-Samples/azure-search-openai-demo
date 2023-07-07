0572986739f72c356bd8e7c749b08f4c51dbae5b
Sri Balakrishnan <srbalakr@microsoft.com>
2023-07-07 14:35:52 -0700

    address cmments

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -19,7 +19,7 @@ class ChatReadRetrieveReadApproach(Approach):
     (answer) with that prompt.
     """
     system_message_chat_conversation = """Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-Answer ONLY with the facts listed in the list of Sources:. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
+Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
 For tabular information return it as an html table. Do not return markdown format.
 Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
 {follow_up_questions_prompt}
@@ -130,7 +130,7 @@ Search query:
         token_count = token_count + self.num_tokens_from_messages(messages, self.chatgpt_model)
         
         #latest conversation
-        userContent = history[-1]["user"] + "\nSources:" + sources
+        userContent = history[-1]["user"] + " \nSources:" + sources
         messages.append({"role": self.USER, "content": userContent})
         token_count = token_count + self.num_tokens_from_messages(messages, self.chatgpt_model)
 
@@ -153,12 +153,11 @@ Search query:
         encoding = tiktoken.encoding_for_model(self.get_oai_chatmodel_tiktok(model))
         num_tokens = 0
         for message in messages:
-            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            num_tokens += 2  # every message follows {role/name}\n{content}\n
             for key, value in message.items():
                 num_tokens += len(encoding.encode(value))
                 if key == "name":  # if there's a name, the role is omitted
                     num_tokens += -1  # role is always required and always 1 token
-        num_tokens += 2  # every reply is primed with <im_start>assistant
         return num_tokens
 
     def get_oai_chatmodel_tiktok(self, aoaimodel: str):
