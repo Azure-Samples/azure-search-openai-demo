42c0eca002de80a34d1eaf02ab4ae4f91294c003
Vraj Routu <kishorervs93@gmail.com>
2023-07-07 17:29:02 -0400

    Update chatreadretrieveread.py

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -1,31 +1,24 @@
-from typing import Any, Sequence
-
 import openai
 from azure.search.documents import SearchClient
 from azure.search.documents.models import QueryType
 from approaches.approach import Approach
 from text import nonewlines
 
+# Simple retrieve-then-read implementation, using the Cognitive Search and OpenAI APIs directly. It first retrieves
+# top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion 
+# (answer) with that prompt.
 class ChatReadRetrieveReadApproach(Approach):
+    system_text = """
+    Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
+    Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
+    For tabular information return it as an html table. Do not return markdown format.
+    Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+    {follow_up_questions_prompt}
+    {injected_prompt}
+    Sources:
+    {sources}
     """
-    Simple retrieve-then-read implementation, using the Cognitive Search and OpenAI APIs directly. It first retrieves
-    top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion
-    (answer) with that prompt.
-    """
-
-    prompt_prefix = """<|im_start|>system
-Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-For tabular information return it as an html table. Do not return markdown format.
-Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
-{follow_up_questions_prompt}
-{injected_prompt}
-Sources:
-{sources}
-<|im_end|>
-{chat_history}
-"""
-
+    
     follow_up_questions_prompt_content = """Generate three very brief follow-up questions that the user would likely ask next about their healthcare plan and employee handbook. 
     Use double angle brackets to reference the questions, e.g. <<Are there exclusions for prescriptions?>>.
     Try not to repeat questions that have already been asked.
@@ -36,15 +29,7 @@ Sources:
     Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
     Do not include any text inside [] or <<>> in the search query terms.
     If the question is not in English, translate the question to English before generating the search query.
-
-Chat History:
-{chat_history}
-
-Question:
-{question}
-
-Search query:
-"""
+    """
 
     def __init__(self, search_client: SearchClient, chatgpt_deployment: str, gpt_deployment: str, sourcepage_field: str, content_field: str):
         self.search_client = search_client
@@ -53,22 +38,29 @@ Search query:
         self.sourcepage_field = sourcepage_field
         self.content_field = content_field
 
-    def run(self, history: Sequence[dict[str, str]], overrides: dict[str, Any]) -> Any:
+    def run(self, history: list[dict], overrides: dict) -> any:
         use_semantic_captions = True if overrides.get("semantic_captions") else False
         top = overrides.get("top") or 3
         exclude_category = overrides.get("exclude_category") or None
         filter = "category ne '{}'".format(exclude_category.replace("'", "''")) if exclude_category else None
 
         # STEP 1: Generate an optimized keyword search query based on the chat history and the last question
-        prompt = self.query_prompt_template.format(chat_history=self.get_chat_history_as_text(history, include_last_turn=False), question=history[-1]["user"])
-        completion = openai.Completion.create(
-            engine=self.gpt_deployment, 
-            prompt=prompt, 
+        prompt_setup = self.query_prompt_template.format(chat_history=self.get_chat_history_as_text(history, include_last_turn=False), question=history[-1]["user"])
+        
+        prompt = [{"role": "system", "content": self.query_prompt_template}]
+        add_history = self.get_chat_history_as_text(history, include_last_turn=False)
+        for step in add_history:
+            prompt.append(step)
+        prompt.append({"role": "user", "content": history[-1]["user"]})
+
+
+        completion = openai.ChatCompletion.create(
+            engine=self.chatgpt_deployment, 
+            messages=prompt, 
             temperature=0.0, 
             max_tokens=32, 
-            n=1, 
-            stop=["\n"])
-        q = completion.choices[0].text
+            n=1)
+        q = completion.choices[0].message.content
 
         # STEP 2: Retrieve relevant documents from the search index with the GPT optimized query
         if overrides.get("semantic_ranker"):
@@ -93,27 +85,31 @@ Search query:
         # Allow client to replace the entire prompt, or to inject into the exiting prompt using >>>
         prompt_override = overrides.get("prompt_template")
         if prompt_override is None:
-            prompt = self.prompt_prefix.format(injected_prompt="", sources=content, chat_history=self.get_chat_history_as_text(history), follow_up_questions_prompt=follow_up_questions_prompt)
+            prompt[0] = {"role": "system", "content": self.system_text.format(injected_prompt="", sources=content, follow_up_questions_prompt=follow_up_questions_prompt)}
         elif prompt_override.startswith(">>>"):
-            prompt = self.prompt_prefix.format(injected_prompt=prompt_override[3:] + "\n", sources=content, chat_history=self.get_chat_history_as_text(history), follow_up_questions_prompt=follow_up_questions_prompt)
+            prompt[0] = {"role": "system", "content": self.system_text.format(injected_prompt=prompt_override[3:] + "\n", sources=content, follow_up_questions_prompt=follow_up_questions_prompt)}
         else:
-            prompt = prompt_override.format(sources=content, chat_history=self.get_chat_history_as_text(history), follow_up_questions_prompt=follow_up_questions_prompt)
+            prompt[0] = {"role": "system", "content": prompt_override.format(sources=content, follow_up_questions_prompt=follow_up_questions_prompt)}
 
         # STEP 3: Generate a contextual and content specific answer using the search results and chat history
-        completion = openai.Completion.create(
+        completion = openai.ChatCompletion.create(
             engine=self.chatgpt_deployment, 
-            prompt=prompt, 
-            temperature=overrides.get("temperature") or 0.7, 
-            max_tokens=1024, 
-            n=1, 
-            stop=["<|im_end|>", "<|im_start|>"])
+            messages=prompt, 
+            temperature=overrides.get("temperature") or 0.5, 
+            max_tokens=2048, 
+            n=1)
 
-        return {"data_points": results, "answer": completion.choices[0].text, "thoughts": f"Searched for:<br>{q}<br><br>Prompt:<br>" + prompt.replace('\n', '<br>')}
+        return {"data_points": results, "answer": completion.choices[0].message.content, "thoughts": f"Searched for:<br>{q}<br><br>Prompt:<br>" + prompt[0]["content"].replace('\n', '<br>')}
     
-    def get_chat_history_as_text(self, history: Sequence[dict[str, str]], include_last_turn: bool=True, approx_max_tokens: int=1000) -> str:
-        history_text = ""
+    def get_chat_history_as_text(self, history, include_last_turn=True, approx_max_tokens=1000):
+        history_text = []
+        total_characters = 0
         for h in reversed(history if include_last_turn else history[:-1]):
-            history_text = """<|im_start|>user""" + "\n" + h["user"] + "\n" + """<|im_end|>""" + "\n" + """<|im_start|>assistant""" + "\n" + (h.get("bot", "") + """<|im_end|>""" if h.get("bot") else "") + "\n" + history_text
-            if len(history_text) > approx_max_tokens*4:
-                break    
+            history_text.append({"role": "user", "content": h["user"]})
+            history_text.append({"role": "assistant", "content": h.get("bot")})
+            
+            for entry in history_text:
+                total_characters += len(entry)
+                if len(history_text) > approx_max_tokens*4:
+                    break    
         return history_text
