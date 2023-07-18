b59ca8df509be552a94c737fa927c3063f4d8553
Pamela Fox <pamela.fox@gmail.com>
2023-07-18 14:46:57 +0000

    Formatting

--- tests/test_prepdocs.py
+++ tests/test_prepdocs.py
@@ -7,7 +7,4 @@ def test_filename_to_id():
     # test filename containing unicode
     assert filename_to_id("foo\u00A9.txt") == "file-foo__txt-666F6FC2A92E747874"
     # test filenaming starting with unicode
-    assert (
-        filename_to_id("ファイル名.pdf")
-        == "file-______pdf-E38395E382A1E382A4E383ABE5908D2E706466"
-    )
+    assert filename_to_id("ファイル名.pdf") == "file-______pdf-E38395E382A1E382A4E383ABE5908D2E706466"
