6b1ae1a5f855599701d49b1ba970d53e53e5de5c
Pamela Fox <pamela.fox@gmail.com>
2023-08-05 23:53:56 +0000

    Get tests working

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
-import asyncio
 from collections import namedtuple
 from unittest import mock
 
-import pytest
+import pytest_asyncio
 
-import app as backend_app
+import app
 from approaches.approach import Approach
 from approaches.chatreadretrieveread import ChatReadRetrieveReadApproach
 
@@ -35,30 +34,20 @@ class MockAzureCredential:
         return MockToken("mock_token", 9999999999)
 
 
-@pytest.fixture()
-def app():
+@pytest_asyncio.fixture
+async def client():
     # mock the DefaultAzureCredential
     with mock.patch("app.DefaultAzureCredential") as mock_default_azure_credential:
         mock_default_azure_credential.return_value = MockAzureCredential()
-        loop = asyncio.get_event_loop()
-        _app = loop.run_until_complete(backend_app.create_app())
+        quart_app = app.create_app()
 
-    _app.config.update(
-        {
-            "TESTING": True,
-            backend_app.CONFIG_ASK_APPROACHES: {"mock": MockedAskApproach()},
-            backend_app.CONFIG_CHAT_APPROACHES: {"mock": MockedChatApproach()},
-        }
-    )
+        async with quart_app.test_app() as test_app:
+            quart_app.config.update(
+                {
+                    "TESTING": True,
+                    app.CONFIG_ASK_APPROACHES: {"mock": MockedAskApproach()},
+                    app.CONFIG_CHAT_APPROACHES: {"mock": MockedChatApproach()},
+                }
+            )
 
-    yield _app
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+            yield test_app.test_client()
