b0b58c69c2bdbee2b96bae85fd7710e5ca523ca9
Pamela Fox <pamela.fox@gmail.com>
2023-08-09 14:58:59 -0700

    Update gunicorn.conf.py

--- app/backend/gunicorn.conf.py
+++ app/backend/gunicorn.conf.py
@@ -3,11 +3,9 @@ import multiprocessing
 max_requests = 1000
 max_requests_jitter = 50
 log_file = "-"
-loglevel = "info"
 bind = "0.0.0.0"
 
 timeout = 600
 num_cpus = multiprocessing.cpu_count()
 workers = (num_cpus * 2) + 1
-threads = 1 if num_cpus == 1 else 2
 worker_class = "uvicorn.workers.UvicornWorker"
