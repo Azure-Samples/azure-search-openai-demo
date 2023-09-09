0a2f73cf1696e7feb48340daae9aa4499408507f
Matt Gotteiner <magottei@microsoft.com>
2023-09-08 17:59:26 -0700

    add view action to manageacl

--- scripts/manageacl.py
+++ scripts/manageacl.py
@@ -35,9 +35,16 @@ def update_acl(credential: AzureKeyCredential|TokenCredential, search_service: s
     if result.get_count() == 0:
         if args.verbose: print(F"No documents match {document} - exiting")
         return
+
+
     
     documents_to_merge = []
     for document in result:
+        if acl_action == "view":
+            # Assumes the acls are consistent across all sections of the document
+            print(document[acl_type])
+            return
+
         if acl_action == "remove":
             new_acls = [acl_value for acl_value in document[acl_type] if acl_value != acl]
         elif acl_action == "add":
@@ -62,7 +69,7 @@ if __name__ == "__main__":
     parser.add_argument("--search-key", required=False, help="Optional. Use this Azure Cognitive Search account key instead of the current user identity to login (use az login to set current user for Azure)")
     parser.add_argument("--enable-acls", required=False, action="store_true", help="Optional. Adds fields required for ACL management to the search index if they aren't already-present")
     parser.add_argument("--acl-type", required=False, choices=["oids", "groups"], help="Optional. Type of ACL")
-    parser.add_argument("--acl-action", required=False, choices=["remove", "add", "remove_all"], help="Optional. Whether to remove or add the ACL to the document")
+    parser.add_argument("--acl-action", required=False, choices=["remove", "add", "view", "remove_all"], help="Optional. Whether to remove or add the ACL to the document")
     parser.add_argument("--acl", required=False, default=None, help="Optional. Value of ACL to add or remove.")
     parser.add_argument("--document", required=False, help="Optional. Name of document to update ACLs for")
     parser.add_argument("--tenant-id", required=False, help="Optional. Use this to define the Azure directory where to authenticate)")
@@ -76,7 +83,7 @@ if __name__ == "__main__":
     if args.enable_acls:
         enable_acls(search_credential, args.search_service, args.index)
     elif args.acl_type and args.acl_action and args.document:
-        if args.acl_type in ["remove", "add"] and args.acl is None:
+        if args.acl_action in ["remove", "add"] and args.acl is None:
             print("Error: --acl must be specified if --acl-action is remove or add")
             exit(1)
 
