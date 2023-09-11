97137be0c21685f5a78a4531aab6d6e2998f6281
soumikbhattacharjee1978 <144156991+soumikbhattacharjee1978@users.noreply.github.com>
2023-09-10 22:09:55 -0400

    Update Layout.tsx

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -38,7 +38,7 @@ const Layout = () => {
                             </li>
                         </ul>
                     </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + Cognitive Search</h4>
+                    <h4 className={styles.headerRightText}>MarketPlace Intelligence(Sandbox)</h4>
                 </div>
             </header>
 
