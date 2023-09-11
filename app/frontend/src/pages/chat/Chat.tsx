120325ae327225e9d058acf2b7499f2536167326
soumikbhattacharjee1978 <144156991+soumikbhattacharjee1978@users.noreply.github.com>
2023-09-10 22:34:30 -0400

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -217,7 +217,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. the sustainability of Microsoft's cloud business?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
