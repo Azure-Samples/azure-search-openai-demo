03c9f9fdd396a6c7e9bd137ce04a4aacdb7ebaa3
soumikbhattacharjee1978 <144156991+soumikbhattacharjee1978@users.noreply.github.com>
2023-09-10 22:59:12 -0400

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -12,7 +12,7 @@ const EXAMPLES: ExampleModel[] = [
         text: "What is the stock price of Microsoft? What is the trend?",
         value: "What is the stock price of Microsoft? What is the trend?"
     },
-    { text: "What is Pfizer outlook on Covid 19?", value: "what is Pfizer outlook on Covid 19?" },
+    { text: "What is Pfizer's outlook on Covid 19?", value: "what is Pfizer outlook on Covid 19?" },
     { text: "Can you provide Biographical information of apple?", value: "Can you provide Biographical information of apple?" }
 ];
 
