ec348c9d7f4ed3c19a5d563b3efc5f35080dba7a
Matt <57731498+mattmsft@users.noreply.github.com>
2023-09-14 21:20:36 +0000

    adding sh files; fixing script errors

---
+++ scripts/manageacl.sh
@@ -0,0 +1,22 @@
+ #!/bin/sh
+
+echo ""
+echo "Loading azd .env file from current environment"
+echo ""
+
+while IFS='=' read -r key value; do
+    value=$(echo "$value" | sed 's/^"//' | sed 's/"$//')
+    export "$key=$value"
+done <<EOF
+$(azd env get-values)
+EOF
+
+echo 'Creating python virtual environment "scripts/.venv"'
+python3 -m venv scripts/.venv
+
+echo 'Installing dependencies from "requirements.txt" into virtual environment'
+./scripts/.venv/bin/python -m pip install -r scripts/requirements.txt
+
+echo "Running manageacl.py. Arguments to script: $@"
+  ./scripts/.venv/bin/python ./scripts/manageacl.py --search-service "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" $@
+  
