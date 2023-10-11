4b0754c23898fa4b45d4b3cd40f9031c462e8f93
Pamela Fox <pamela.fox@gmail.com>
2023-10-11 22:39:41 +0000

    Initial launch improvements

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -25,6 +25,8 @@ export default defineConfig({
     },
     server: {
         proxy: {
+            "/content/": "http://localhost:50505",
+            "/auth_setup": "http://localhost:50505",
             "/ask": "http://localhost:50505",
             "/chat": "http://localhost:50505"
         }
