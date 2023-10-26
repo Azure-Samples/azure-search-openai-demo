3acccb813cff26b1b52a05894c1a998b6547c821
Pamela Fox <pamela.fox@gmail.com>
2023-10-26 23:53:29 +0000

    Make mypy happy with explicit check of path

--- scripts/prepdocslib/blobmanager.py
+++ scripts/prepdocslib/blobmanager.py
@@ -52,8 +52,8 @@ class BlobManager:
                 blobs = container_client.list_blob_names(name_starts_with=os.path.splitext(os.path.basename(prefix))[0])
             async for blob_path in blobs:
                 # This still supports PDFs split into individual pages, but we could remove in future to simplify code
-                if prefix is not None and not (
-                    re.match(rf"{prefix}-\d+\.pdf", blob_path) or blob_path == os.path.basename(path)
+                if (prefix is not None and not re.match(rf"{prefix}-\d+\.pdf", blob_path)) or (
+                    path is not None and blob_path == os.path.basename(path)
                 ):
                     continue
                 if self.verbose:
