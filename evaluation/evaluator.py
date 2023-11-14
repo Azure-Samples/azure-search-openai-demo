3e40f5bc6253062ce444749241c90c737b140633
Pamela Fox <pamela.fox@gmail.com>
2023-11-14 00:44:44 +0000

    Latest results

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -175,13 +175,13 @@ def run_evaluation(testdata_filename, destination_dir, overrides={}):
 if __name__ == "__main__":
     run_evaluation(
         "input/qa.jsonl",
-        destination_dir=EVAL_DIR / "results/no_semantic_ranker_prompt_chris3",
+        destination_dir=EVAL_DIR / "results/no_semantic_ranker_prompt_christrim2",
         overrides={
             "retrieval_mode": "hybrid",
             "semantic_ranker": False,
             "semantic_captions": False,
             "top": 3,
             "suggest_followup_questions": False,
-            "prompt_template": open(EVAL_DIR / "input/prompt_chris.txt").read(),
+            "prompt_template": open(EVAL_DIR / "input/prompt_christrim.txt").read(),
         },
     )
