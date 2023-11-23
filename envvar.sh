e4c4a65ecb38debed0ac529e1bfe59e27fc6b5de
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2023-11-23 09:15:28 +0100

    asdasd

--- envvar.sh
+++ envvar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Run azd env set AZURE_RESOURCE_GROUP {Name of existing resource group}
-Run azd env set AZURE_LOCATION {Location of existing resource group}
+Run azd env set AZURE_RESOURCE_GROUP AI-Dev
+Run azd env set AZURE_LOCATION westeurope
 azd env set OPENAI_HOST openai
-azd env set OPENAI_ORGANIZATION {Your OpenAI organization}
-azd env set OPENAI_API_KEY {Your OpenAI API key}
+azd env set OPENAI_ORGANIZATION org-id
+azd env set OPENAI_API_KEY asdmasdmf9
