72f0a13d94bf6a89ff0150ebdf94d29b7bf258bd
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2023-11-28 13:36:58 +0100

    change var file

--- envvar.sh
+++ envvar.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 azd env set AZURE_RESOURCE_GROUP AI-Dev
 azd env set AZURE_LOCATION westeurope
-azd env set OPENAI_HOST openai
-azd env set OPENAI_ORGANIZATION org-id
-azd env set OPENAI_API_KEY asdmasdmf9
