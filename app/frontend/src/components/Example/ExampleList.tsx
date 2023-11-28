a6aed9a5d15dc8a29ad75c094eff0c0cf31758d0
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2023-11-28 13:51:01 +0000

    message

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -12,8 +12,7 @@ const EXAMPLES: ExampleModel[] = [
         text: "Summerize a cv for me",
         value: "Summerize a cv for me"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Find a person with cloud competancy", value: "Find a person with cloud competancy" }
 ];
 
 interface Props {
