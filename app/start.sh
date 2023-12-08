d53129a1f077e87b014bfdffcb81134565daa9e1
Adnan Ashraf <adnan.newton@gmail.com>
2023-12-08 16:01:56 +0500

    fixed

--- app/start.sh
+++ app/start.sh
@@ -24,32 +24,32 @@ echo "Restoring backend python packages"
 echo ""
 
 cd backend
-./backend_env/bin/python -m pip install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Failed to restore backend python packages"
-    exit $?
-fi
+# ./backend_env/bin/python -m pip install -r requirements.txt
+# if [ $? -ne 0 ]; then
+#     echo "Failed to restore backend python packages"
+#     exit $?
+# fi
 
 echo ""
 echo "Restoring frontend npm packages"
 echo ""
 
 cd ../frontend
-npm install
-if [ $? -ne 0 ]; then
-    echo "Failed to restore frontend npm packages"
-    exit $?
-fi
+# npm install
+# if [ $? -ne 0 ]; then
+#     echo "Failed to restore frontend npm packages"
+#     exit $?
+# fi
 
 echo ""
 echo "Building frontend"
 echo ""
 
-npm run build
-if [ $? -ne 0 ]; then
-    echo "Failed to build frontend"
-    exit $?
-fi
+# npm run build
+# if [ $? -ne 0 ]; then
+#     echo "Failed to build frontend"
+#     exit $?
+# fi
 
 echo ""
 echo "Starting backend"
