22ed4e4d33d583e521da3ae015efe7b271d506d1
Adnan Ashraf <adnan.newton@gmail.com>
2023-12-08 19:47:54 +0500

    fixed

--- app/frontend/src/api/api.ts
+++ app/frontend/src/api/api.ts
@@ -42,6 +42,7 @@ export async function chatApi(request: ChatAppRequest, idToken: string | undefin
 
 export async function uploadFilesApi(request: UploadFilesRequest, idToken: string | undefined): Promise<Response> {
     const headers = getHeaders(idToken);
+    headers["Content-Type"] = "multipart/form-data";
     return await fetch(`${BACKEND_URI}/upload`, {
         method: "POST",
         headers: headers,
