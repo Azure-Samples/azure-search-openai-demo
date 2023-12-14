84a9e79f3a498d042916ba15ec2a8d4e61d77f89
Pamela Fox <pamela.fox@gmail.com>
2023-12-14 21:14:46 +0000

    Handle font not found

--- scripts/prepdocslib/blobmanager.py
+++ scripts/prepdocslib/blobmanager.py
@@ -81,6 +81,7 @@ class BlobManager:
             new_img.paste(original_img, (0, text_height))
 
             # Draw the text on the white area
+            font = None
             try:
                 font = ImageFont.truetype("arial.ttf", 20)
             except OSError:
