a738d9f6322da960222f9bb43214c556521b3bd8
Henri Ossi <henri.ossi@sitowise.com>
2024-03-02 16:03:48 +0200

    Fetch PDF files to data collections locally

---
+++ scripts/data-collections/fetch-data-collections.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Processes PDF link collections in current directory,
+# and downloads them to respective subfolders 
+
+# Check that required commands exist on user system
+if ! command -v wget &> /dev/null;
+then
+  echo "This script needs wget to run!"
+  exit 1
+fi
+
+if ! command -v basename &> /dev/null;
+then
+  echo "This script needs basename to run!"
+  exit 1
+fi
+
+# Find and process link collections
+for filename in ./*.urls; do
+  collectionname=$(basename "${filename}" .urls)
+  echo "Fetching collection: ${collectionname}"
+
+  outputdir="../../data/collections/${collectionname}"
+  mkdir -p "${outputdir}"
+
+  # Rate limit to 500kB/s and wait 2 seconds between each file.
+  wget -i "${filename}" -nc -P "${outputdir}" \
+    -e robots=on --accept '*.pdf' \
+    --limit-rate=500k --wait=2 \
+    -q --show-progress
+
+done
