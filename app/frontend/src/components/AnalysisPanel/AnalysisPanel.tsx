6c31844ff3e5debfc1e2297419cd6140bf883eb7
Yuval Yaron <yuvalyaron@microsoft.com>
2024-03-07 14:40:50 +0000

    fix indentation

--- app/frontend/src/components/AnalysisPanel/AnalysisPanel.tsx
+++ app/frontend/src/components/AnalysisPanel/AnalysisPanel.tsx
@@ -29,46 +29,46 @@ export const AnalysisPanel = ({ answer, activeTab, activeCitation, citationHeigh
     const isDisabledCitationTab: boolean = !activeCitation;
     const [citation, setCitation] = useState("");
 
-        const client = useLogin ? useMsal().instance : undefined;
+    const client = useLogin ? useMsal().instance : undefined;
 
-        const fetchCitation = async () => {
-            const token = client ? await getToken(client) : undefined;
-            if (activeCitation) {
-                // Get hash from the URL as it may contain #page=N
-                // which helps browser PDF renderer jump to correct page N
-                const originalHash = activeCitation.indexOf("#") ? activeCitation.split("#")[1] : "";
-                const response = await fetch(activeCitation, {
-                    method: "GET",
-                    headers: getHeaders(token)
-                });
-                const citationContent = await response.blob();
-                let citationObjectUrl = URL.createObjectURL(citationContent);
-                // Add hash back to the new blob URL
-                if (originalHash) {
-                    citationObjectUrl += "#" + originalHash;
-                }
-                setCitation(citationObjectUrl);
+    const fetchCitation = async () => {
+        const token = client ? await getToken(client) : undefined;
+        if (activeCitation) {
+            // Get hash from the URL as it may contain #page=N
+            // which helps browser PDF renderer jump to correct page N
+            const originalHash = activeCitation.indexOf("#") ? activeCitation.split("#")[1] : "";
+            const response = await fetch(activeCitation, {
+                method: "GET",
+                headers: getHeaders(token)
+            });
+            const citationContent = await response.blob();
+            let citationObjectUrl = URL.createObjectURL(citationContent);
+            // Add hash back to the new blob URL
+            if (originalHash) {
+                citationObjectUrl += "#" + originalHash;
             }
-        };
-        useEffect(() => {
-            fetchCitation();
-        }, []);
+            setCitation(citationObjectUrl);
+        }
+    };
+    useEffect(() => {
+        fetchCitation();
+    }, []);
 
-        const renderFileViewer = () => {
-            if (!activeCitation) {
-                return null;
-            }
-        
-            const fileExtension = activeCitation.split(".").pop()?.toLowerCase();
-            switch (fileExtension) {
-                case "png":
-                    return <img src={citation} className={styles.citationImg} alt="Citation Image" />;
-                case "md":
-                    return <MarkdownViewer src={activeCitation} />;
-                default:
-                    return <iframe title="Citation" src={citation} width="100%" height={citationHeight} />;
-            }
-        };
+    const renderFileViewer = () => {
+        if (!activeCitation) {
+            return null;
+        }
+
+        const fileExtension = activeCitation.split(".").pop()?.toLowerCase();
+        switch (fileExtension) {
+            case "png":
+                return <img src={citation} className={styles.citationImg} alt="Citation Image" />;
+            case "md":
+                return <MarkdownViewer src={activeCitation} />;
+            default:
+                return <iframe title="Citation" src={citation} width="100%" height={citationHeight} />;
+        }
+    };
 
     return (
         <Pivot
