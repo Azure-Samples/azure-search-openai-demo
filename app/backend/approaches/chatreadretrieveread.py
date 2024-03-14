a65ca6028edf60251c8debca3f4954b92dab3a10
czumbiehl <159044748+czumbiehl@users.noreply.github.com>
2024-03-14 15:48:06 +0000

    before adding upload

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -58,7 +58,7 @@ class ChatReadRetrieveReadApproach(ChatApproach):
         Guide to use the software, 
         Provide advanced tips to optimize your use of the software.
         Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-        For tabular information return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.
+        For tabular information return it as an html table. Return Markdown format if needed for lists and tables. If the question is not in English, answer in the language used in the question.
         Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, for example [info1.txt]. Don't combine sources, list each source separately, for example [info1.txt][info2.pdf].
         {follow_up_questions_prompt}
         {injected_prompt}
