a4a0e803d6e27cb34a6a26d762b5021b23744128
John Aziz <johnaziz269@gmail.com>
2024-06-01 20:51:11 +0000

    improve and refactor code

--- app/frontend/src/components/Answer/SpeechOutputBrowser.tsx
+++ app/frontend/src/components/Answer/SpeechOutputBrowser.tsx
@@ -15,51 +15,61 @@ try {
     console.error("SpeechSynthesis is not supported");
 }
 
-export const SpeechOutputBrowser = ({ answer }: Props) => {
-    const [isSpeaking, SetIsSpeaking] = useState<boolean>(false);
+const getUtterance = function (text: string) {
+    if (synth) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = "en-US";
+        utterance.volume = 1;
+        utterance.rate = 1;
+        utterance.pitch = 1;
+        utterance.voice = synth.getVoices().filter((voice: SpeechSynthesisVoice) => voice.lang === "en-US")[0];
+        return utterance;
+    }
+};
 
-    const utterance = new SpeechSynthesisUtterance(answer);
-    const onSpeechSynthesisClicked = (utterance: SpeechSynthesisUtterance) => {
+export const SpeechOutput = ({ answer }: Props) => {
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    if (!synth) {
+        return <></>;
+    }
+
+    const startOrStopSpeech = (answer: string) => {
         if (synth != null) {
-            utterance.lang = "en-US";
-            utterance.volume = 1;
-            utterance.rate = 1;
-            utterance.pitch = 1;
-            utterance.voice = synth.getVoices().filter((voice: SpeechSynthesisVoice) => voice.lang === "en-US")[0];
-            console.log(utterance.voice);
+            if (isPlaying) {
+                synth.cancel(); // removes all utterances from the utterance queue.
+                setIsPlaying(false);
+                return;
+            }
+            const utterance: SpeechSynthesisUtterance | undefined = getUtterance(answer);
+
+            if (!utterance) {
+                return;
+            }
 
             synth.speak(utterance);
 
             utterance.onstart = () => {
-                SetIsSpeaking(true);
+                setIsPlaying(true);
+                return;
             };
+
             utterance.onend = () => {
-                SetIsSpeaking(false);
+                setIsPlaying(false);
+                return;
             };
         }
     };
-    // The state of this is managed by the parent component, to ensure that
-    // only one speech is outputted at any given time.
+    const color = isPlaying ? "red" : "black";
+
     return (
-        <>
-            {isSpeaking && (
-                <IconButton
-                    style={{ color: "red" }}
-                    iconProps={{ iconName: "Volume3" }}
-                    title="Speak answer"
-                    ariaLabel="Speak answer"
-                    onClick={() => onSpeechSynthesisClicked(utterance)}
-                />
-            )}
-            {!isSpeaking && (
-                <IconButton
-                    style={{ color: "black" }}
-                    iconProps={{ iconName: "Volume3" }}
-                    title="Speak answer"
-                    ariaLabel="Speak answer"
-                    onClick={() => onSpeechSynthesisClicked(utterance)}
-                />
-            )}
-        </>
+        <IconButton
+            style={{ color: color }}
+            iconProps={{ iconName: "Volume3" }}
+            title="Speak answer"
+            ariaLabel="Speak answer"
+            onClick={() => startOrStopSpeech(answer)}
+            disabled={!synth}
+        />
     );
 };
