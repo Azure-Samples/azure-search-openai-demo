086385717a28425b4953fc2ca9196933102b3971
lana-noor <lananoor@microsoft.com>
2024-06-12 21:53:59 +0400

    layout back to original

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
 
-import alGhurairLogo from "../../assets/AGILogo.png";
+import github from "../../assets/github.svg";
 
 import styles from "./Layout.module.css";
 
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>Al Ghurair Policy Assistant | Demo</h3>
+                        <h3 className={styles.headerTitle}> Al Ghurair Policy Assistant | Demo</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -29,14 +29,14 @@ const Layout = () => {
                                 </NavLink>
                             </li>
                             <li className={styles.headerNavLeftMargin}>
-                                <a href="https://www.al-ghurair.com/en/our-company/about-us" target={"_blank"} title="Al Ghurair website link">
+                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
                                     <img
-                                        src={alGhurairLogo}
-                                        alt="Al Ghurair logo"
-                                        aria-label="Link to Al Ghurair website"
+                                        src={github}
+                                        alt="Github logo"
+                                        aria-label="Link to github repository"
                                         width="20px"
                                         height="20px"
-                                        className={styles.AGILogo}
+                                        className={styles.githubLogo}
                                     />
                                 </a>
                             </li>
