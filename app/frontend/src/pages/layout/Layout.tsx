8282454a9666e03f2c0d9011834c3e6aeaa3df66
lana-noor <lananoor@microsoft.com>
2024-07-14 17:19:56 +0400

    Change title frontend app

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}> Al Ghurair Policy Assistant | Demo</h3>
+                        <h3 className={styles.headerTitle}> Policy & Health Policy Assistant| Demo</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
