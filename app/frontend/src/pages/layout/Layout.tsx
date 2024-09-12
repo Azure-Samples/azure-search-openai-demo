48facf5996077235d4cdf46dfda17c9d88e467a4
lana-noor <lananoor@microsoft.com>
2024-09-12 11:33:50 +0400

    layout change title

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Outlet, NavLink, Link } from "react-router-dom";
 
 import github from "../../assets/github.svg";
@@ -54,60 +53,3 @@ const Layout = () => {
 };
 
 export default Layout;
-=======
-import { Outlet, NavLink, Link } from "react-router-dom";
-
-import github from "../../assets/github.svg";
-
-import styles from "./Layout.module.css";
-
-import { useLogin } from "../../authConfig";
-
-import { LoginButton } from "../../components/LoginButton";
-
-const Layout = () => {
-    return (
-        <div className={styles.layout}>
-            <header className={styles.header} role={"banner"}>
-                <div className={styles.headerContainer}>
-                    <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}> Health Assistant | Demo</h3>
-                    </Link>
-                    <nav>
-                        <ul className={styles.headerNavList}>
-                            <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
-                                    <img
-                                        src={github}
-                                        alt="Github logo"
-                                        aria-label="Link to github repository"
-                                        width="20px"
-                                        height="20px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
-                    {useLogin && <LoginButton />}
-                </div>
-            </header>
-
-            <Outlet />
-        </div>
-    );
-};
-
-export default Layout;
->>>>>>> 348f026a3e2175bd37d0cc254a8a72c386503330
