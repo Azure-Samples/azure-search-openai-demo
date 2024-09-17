f44d595f018d2826b0037ae4ed6950e2bf3eebb6
lana-noor <lananoor@microsoft.com>
2024-09-17 17:22:30 +0400

    chat2

--- app/backend/approaches/chatreadretrievereadvision.py
+++ app/backend/approaches/chatreadretrievereadvision.py
@@ -68,7 +68,7 @@ class ChatReadRetrieveReadVisionApproach(ChatApproach):
     @property
     def system_message_chat_conversation(self):
         return """
-        You are an intelligent assistant helping doctors, nurses and hospital staff understand certain topics in medicine. The documents contain text, graphs, tables and images.
+        You are an intelligent assistant helping users understand the policies and regulations at the Dubai Health Authority. The documents contain text, graphs, tables and images
         Each image source has the file name in the top left corner of the image with coordinates (10,10) pixels and is in the format SourceFileName:<file_name>
         Each text source starts in a new line and has the file name followed by colon and the actual information
         Always include the source name from the image or text for each fact you use in the response in the format: [filename]
