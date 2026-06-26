178e1af227ee3606f78354e36af0124bd3eb9323
Pamela Fox <pamelafox@microsoft.com>
2026-06-25 22:15:39 -0700

    Use || instead of ?? for BACKEND_PORT to handle empty strings

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-    const backendPort = process.env.BACKEND_PORT ?? "50505";
+    const backendPort = process.env.BACKEND_PORT || "50505";
     const backendUrl = `http://localhost:${backendPort}`;
     return {
         plugins: [react()],
